f82073339384d1d00f8c097676d831d1710e1ef3
Bebo-Maker <b.fenner@core-soft.de>
2021-03-02 15:30:50 +0100

    Added ActivatorObjectCreator.

---
+++ src/Orm/ObjectCreator/ActivatorObjectCreator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Orm.ObjectCreator
+{
+  public class ActivatorObjectCreator : IObjectCreator
+  {
+    public T Create<T>(Type type) => (T)Activator.CreateInstance(type);
+
+    public T Create<T>(Type type, object[] parameters) => (T)Activator.CreateInstance(type, parameters);
+  }
+}
