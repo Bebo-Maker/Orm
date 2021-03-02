308c6423f779649a6f3a27262659eb4645f48b01
Bebo-Maker <b.fenner@core-soft.de>
2021-03-02 15:20:55 +0100

    Refactoring Database.

--- src/Orm/Database.cs
+++ src/Orm/Database.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace Orm
@@ -40,7 +41,7 @@ namespace Orm
     private static List<T> CreateObjects<T>(SqlDataReader reader)
     {
       var type = typeof(T);
-      var ctor = type.GetConstructors().FirstOrDefault(c => c.CustomAttributes.FirstOrDefault(a => a.AttributeType == typeof(DbConstructorAttribute)) != null);
+      var ctor = GetConstructorWithDbConstructorAttribute(type);
 
       return ctor != null
           ? HandleConstructorInjection<T>(reader, type, ctor.GetParameters().Select(a => a.Name.ToLower()).ToArray())
@@ -50,11 +51,23 @@ namespace Orm
     private static Task<List<T>> CreateObjectsAsync<T>(SqlDataReader reader)
     {
       var type = typeof(T);
-      var ctor = type.GetConstructors().FirstOrDefault(c => c.CustomAttributes.FirstOrDefault(a => a.AttributeType == typeof(DbConstructorAttribute)) != null);
+      var ctor = GetConstructorWithDbConstructorAttribute(type);
 
       return ctor != null
           ? HandleConstructorInjectionAsync<T>(reader, type, ctor.GetParameters().Select(a => a.Name.ToLower()).ToArray())
           : HandlePropertyInjectionAsync<T>(reader, type);
+    }
+
+    private static ConstructorInfo GetConstructorWithDbConstructorAttribute(Type type) => type.GetConstructors().FirstOrDefault(c => c.CustomAttributes.FirstOrDefault(a => a.AttributeType == typeof(DbConstructorAttribute)) != null);
+
+    private static List<T> HandleConstructorInjection<T>(SqlDataReader reader, Type type, string[] parameters)
+    {
+      var entities = new List<T>();
+
+      while (reader.Read())
+        entities.Add(CreateEntity<T>(reader, type, parameters));
+
+      return entities;
     }
 
     private static async Task<List<T>> HandleConstructorInjectionAsync<T>(SqlDataReader reader, Type type, string[] parameters)
@@ -62,20 +75,35 @@ namespace Orm
       var entities = new List<T>();
 
       while (await reader.ReadAsync().ConfigureAwait(false))
-      {
-        var dbData = new object[reader.FieldCount];
-        for (int i = 0; i < reader.FieldCount; i++)
-        {
-          string fieldName = reader.GetName(i).ToLower();
-          int index = Array.IndexOf(parameters, fieldName);
-          if (index != -1)
-            dbData[index] = reader[i];
-          else
-            Console.WriteLine($"ERROR no matching paramter for field {fieldName}");
-        }
+        entities.Add(CreateEntity<T>(reader, type, parameters));
 
-        entities.Add((T)Activator.CreateInstance(type, dbData));
+      return entities;
+    }
+
+
+    private static T CreateEntity<T>(SqlDataReader reader, Type type, string[] parameters)
+    {
+      var dbData = new object[reader.FieldCount];
+      for (int i = 0; i < reader.FieldCount; i++)
+      {
+        string fieldName = reader.GetName(i).ToLower();
+        int index = Array.IndexOf(parameters, fieldName);
+        if (index != -1)
+          dbData[index] = reader[i];
+        else
+          throw new ArgumentException($"No matching parameter for field {fieldName}");
       }
+
+      return ((T)Activator.CreateInstance(type, dbData));
+    }
+
+    private static List<T> HandlePropertyInjection<T>(SqlDataReader reader, Type type)
+    {
+      var entities = new List<T>();
+      var propertyMap = PropertyMap.GetOrCreatePropertyMap<T>();
+
+      while (reader.Read())
+        entities.Add(CreateEntity<T>(reader, type, propertyMap));
 
       return entities;
     }
@@ -85,64 +113,22 @@ namespace Orm
       var entities = new List<T>();
       var propertyMap = PropertyMap.GetOrCreatePropertyMap<T>();
 
-      T entity;
-
       while (await reader.ReadAsync().ConfigureAwait(false))
-      {
-        entity = (T)Activator.CreateInstance(type);
-        for (int i = 0; i < reader.FieldCount; i++)
-        {
-          string fieldName = reader.GetName(i);
-          propertyMap[fieldName]?.SetValue(entity, reader[i]);
-        }
-        entities.Add(entity);
-      }
+        entities.Add(CreateEntity<T>(reader, type, propertyMap));
 
       return entities;
     }
 
-    private static List<T> HandleConstructorInjection<T>(SqlDataReader reader, Type type, string[] parameters)
+    private static T CreateEntity<T>(SqlDataReader reader, Type type, Dictionary<string, FastPropertyInfo> propertyMap)
     {
-      var entities = new List<T>();
-
-      while (reader.Read())
+      T entity = (T)Activator.CreateInstance(type);
+      for (int i = 0; i < reader.FieldCount; i++)
       {
-        var dbData = new object[reader.FieldCount];
-        for (int i = 0; i < reader.FieldCount; i++)
-        {
-          string fieldName = reader.GetName(i).ToLower();
-          int index = Array.IndexOf(parameters, fieldName);
-          if (index != -1)
-            dbData[index] = reader[i];
-          else
-            Console.WriteLine($"ERROR no matching paramter for field {fieldName}");
-        }
-
-        entities.Add((T)Activator.CreateInstance(type, dbData));
+        string fieldName = reader.GetName(i);
+        propertyMap[fieldName]?.SetValue(entity, reader[i]);
       }
 
-      return entities;
-    }
-
-    private static List<T> HandlePropertyInjection<T>(SqlDataReader reader, Type type)
-    {
-      var entities = new List<T>();
-      var propertyMap = PropertyMap.GetOrCreatePropertyMap<T>();
-
-      T entity;
-
-      while (reader.Read())
-      {
-        entity = (T)Activator.CreateInstance(type);
-        for (int i = 0; i < reader.FieldCount; i++)
-        {
-          string fieldName = reader.GetName(i);
-          propertyMap[fieldName]?.SetValue(entity, reader[i]);
-        }
-        entities.Add(entity);
-      }
-
-      return entities;
+      return entity;
     }
   }
 }
