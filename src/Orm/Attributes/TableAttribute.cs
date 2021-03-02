1f1429541a589f95ec4b4be2dd68a08e6be1f9f1
Bebo-Maker <b.fenner@core-soft.de>
2021-03-02 18:06:25 +0100

    Add TableAttribute.

---
+++ src/Orm/Attributes/TableAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Orm.Attributes
+{
+  [AttributeUsage(AttributeTargets.Class)]
+  public class TableAttribute : Attribute
+  {
+    public string Alias { get; }
+
+    public TableAttribute(string alias)
+    {
+      Alias = alias;
+    }
+  }
+}
