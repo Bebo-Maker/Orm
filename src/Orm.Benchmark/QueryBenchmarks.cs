a688db7b748a6cb087893da535dabda886178cf9
Bebo-Maker <b.fenner@core-soft.de>
2021-03-02 12:59:23 +0100

    Added Benchmarks for Query Async.

--- src/Orm.Benchmark/QueryBenchmarks.cs
+++ src/Orm.Benchmark/QueryBenchmarks.cs
@@ -1,11 +1,14 @@
 ﻿using BenchmarkDotNet.Attributes;
 using Orm.Entities;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Orm.Benchmarks
 {
   public class QueryBenchmarks
   {
+    private const string SelectAllFromTestTable = "SELECT [Name],[Id],[Datetime],[Number],[LongText] FROM[TestDB].[dbo].[TestTable]";
+
     private readonly Database _db;
 
     public QueryBenchmarks()
@@ -14,9 +17,16 @@ namespace Orm.Benchmarks
     }
 
     [Benchmark]
-    public List<TestData> QueryProperties() => _db.Query<TestData>("SELECT [Name],[Id],[Datetime],[Number],[LongText] FROM[TestDB].[dbo].[TestTable]");
+    public List<TestData> QueryProperties() => _db.Query<TestData>(SelectAllFromTestTable);
 
     [Benchmark]
-    public List<ConstructorTestData> QueryConstructor() => _db.Query<ConstructorTestData>("SELECT [Name],[Id],[Datetime],[Number],[LongText] FROM[TestDB].[dbo].[TestTable]");
+    public List<ConstructorTestData> QueryConstructor() => _db.Query<ConstructorTestData>(SelectAllFromTestTable);
+
+
+    [Benchmark]
+    public async Task<List<TestData>> QueryPropertiesAsync() => await _db.QueryAsync<TestData>(SelectAllFromTestTable);
+
+    [Benchmark]
+    public async Task<List<ConstructorTestData>> QueryConstructorAsync() => await _db.QueryAsync<ConstructorTestData>(SelectAllFromTestTable);
   }
 }
