b0651bcb80d1b9c72971a7620596138076e17364
Bebo-Maker <b.fenner@core-soft.de>
2021-03-02 12:54:40 +0100

    Init Benchmarks.

---
+++ src/Orm.Benchmark/QueryBenchmarks.cs
@@ -0,0 +1,22 @@
+﻿using BenchmarkDotNet.Attributes;
+using Orm.Entities;
+using System.Collections.Generic;
+
+namespace Orm.Benchmarks
+{
+  public class QueryBenchmarks
+  {
+    private readonly Database _db;
+
+    public QueryBenchmarks()
+    {
+      _db = Database.Connect(@"Server=localhost\SQLEXPRESS;Database=TestDB;Trusted_Connection=True;");
+    }
+
+    [Benchmark]
+    public List<TestData> QueryProperties() => _db.Query<TestData>("SELECT [Name],[Id],[Datetime],[Number],[LongText] FROM[TestDB].[dbo].[TestTable]");
+
+    [Benchmark]
+    public List<ConstructorTestData> QueryConstructor() => _db.Query<ConstructorTestData>("SELECT [Name],[Id],[Datetime],[Number],[LongText] FROM[TestDB].[dbo].[TestTable]");
+  }
+}
