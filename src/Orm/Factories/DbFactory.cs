24839bbc18bcb044d9651b09a1d25527550843ea
Bebo-Maker <b.fenner@core-soft.de>
2021-03-07 17:43:01 +0100

    Create DbFactory.cs

---
+++ src/Orm/Factories/DbFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data;
+using System.Data.Common;
+
+namespace Orm.Factories
+{
+  public class DbFactory
+  {
+    public static IDbCommand CreateCommand(IDbConnection connection, string sqlStatement)
+    {
+      var command = connection.CreateCommand();
+      command.CommandText = sqlStatement;
+      return command;
+    }
+
+    public static DbCommand TryCreateAsyncComand(IDbConnection connection, string sqlStatement)
+    {
+      var cmd = connection.CreateCommand();
+      if (cmd is not DbCommand command)
+        throw new InvalidOperationException($"{cmd} has to be DbCommand");
+
+      command.CommandText = sqlStatement;
+      return command;
+    }
+  }
+}
