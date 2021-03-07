fe29f5b2efa3bce3c9a6164ab6eb41c43a66a190
Bebo-Maker <b.fenner@core-soft.de>
2021-03-07 13:44:51 +0100

    Added sql translator.

---
+++ src/Orm/Querying/ISqlTranslator.cs
@@ -0,0 +1,10 @@
+﻿namespace Orm.Querying
+{
+  public interface ISqlTranslator
+  {
+    string Where(string condition);
+    string OrderBy(string columnName);
+    string OrderByDescending(string columnName);
+    string AppendColumn(string column);
+  }
+}
