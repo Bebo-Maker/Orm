267f4a8e0f1eb1e917810d1d397e89d16f12327e
Bebo-Maker <b.fenner@core-soft.de>
2021-03-07 13:46:51 +0100

    Implemented QueryBuilder.

---
+++ src/Orm/Querying/Builders/SelectQueryBuilder.cs
@@ -0,0 +1,18 @@
+﻿namespace Orm.Querying.Builders
+{
+  public class SelectQueryBuilder<T> : QueryBuilder<T>
+  {
+    public SelectQueryBuilder(ISqlTranslator translator) : base(translator)
+    { 
+      _sb.Append("SELECT ");
+
+      var columns = Table.ColumnNames;
+      for (int i = 0; i < columns.Length - 1; i++)
+        _sb.Append(columns[i]).Append(", ");
+
+      _sb.Append(columns[^1]);
+
+      AppendFrom();
+    }
+  }
+}
