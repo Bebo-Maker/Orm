82d1d76a8743421a569c63d983fe3f7e407a32dc
Bebo-Maker <b.fenner@core-soft.de>
2021-03-07 13:44:39 +0100

    Added utils for accessing alias of table.

---
+++ src/Orm/Utils/AliasUtils.cs
@@ -0,0 +1,12 @@
+﻿using Orm.Attributes;
+using System;
+using System.Reflection;
+
+namespace Orm.Utils
+{
+  public static class AliasUtils
+  {
+    public static string GetTableName(Type type) => type.GetCustomAttribute<TableAttribute>()?.Alias ?? type.Name;
+    public static string GetColumnName(PropertyInfo prop) => prop.GetCustomAttribute<ColumnAttribute>()?.Alias ?? prop.Name;
+  }
+}
