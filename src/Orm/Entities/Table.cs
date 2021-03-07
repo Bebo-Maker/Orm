179adac722155102fbd17e215a0d08665b209ccb
Bebo-Maker <b.fenner@core-soft.de>
2021-03-07 13:45:51 +0100

    Rename TableDefinition -> Table.

--- src/Orm/Entities/Table.cs
+++ src/Orm/Entities/Table.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 
 namespace Orm.Entities
 {
-  public class TableDefinition
+  public class Table
   {
     public string Name { get; }
 
+    public string[] ColumnNames { get; }
+
     public Dictionary<string, FastPropertyInfo> Columns { get; }
 
-    public TableDefinition(string name, Dictionary<string, FastPropertyInfo> columns)
+    public Table(string name, string[] columnNames, Dictionary<string, FastPropertyInfo> columns)
     {
       Name = name;
+      ColumnNames = columnNames;
       Columns = columns;
     }
   }
