f23506b6ef957a999bd231ca0ebf170e94fa1b47
Bebo-Maker <b.fenner@core-soft.de>
2021-03-07 14:49:15 +0100

    Add column entity, refactor creating set method.

---
+++ src/Orm/Entities/Column.cs
@@ -0,0 +1,18 @@
+﻿using Orm.Reflection;
+
+namespace Orm.Entities
+{
+  public class Column
+  {
+    public string Name { get; }
+    public string Alias { get; }
+    public SetValueDelegate SetValue { get; }
+
+    public Column(string name, string alias, SetValueDelegate setValue)
+    {
+      Name = name;
+      Alias = alias;
+      SetValue = setValue;
+    }
+  }
+}
