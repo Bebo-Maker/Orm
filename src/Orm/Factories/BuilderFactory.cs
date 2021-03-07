a0bb0993b549f2750606d36998376a33b7e18a97
Bebo-Maker <b.fenner@core-soft.de>
2021-03-07 18:53:10 +0100

    Create BuilderFactory.cs

---
+++ src/Orm/Factories/BuilderFactory.cs
@@ -0,0 +1,19 @@
+﻿using Orm.Querying;
+using Orm.Querying.Builders;
+using System;
+
+namespace Orm.Factories
+{
+  internal class BuilderFactory
+  {
+    public static string CreateSelectBuilder<T>(Action<IQueryBuilder<T>> action) => BuildBuilder(action, new SelectQueryBuilder<T>());
+    public static string CreateSelectDistinctBuilder<T>(Action<IQueryBuilder<T>> action) => BuildBuilder(action, new SelectDistinctQueryBuilder<T>());
+    public static string CreateDeleteBuilder<T>(Action<IQueryBuilder<T>> action) => BuildBuilder(action, new DeleteQueryBuilder<T>());
+
+    private static string BuildBuilder<T>(Action<IQueryBuilder<T>> action, QueryBuilder<T> builder)
+    {
+      action?.Invoke(builder);
+      return builder.Build();
+    }
+  }
+}
