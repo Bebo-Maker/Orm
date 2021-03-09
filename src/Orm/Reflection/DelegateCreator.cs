46e173e73bf75272ef8a516b78093262dfc497b5
Bebo-Maker <b.fenner@core-soft.de>
2021-03-09 19:02:29 +0100

    Added CreatePropertyGetter to DelegateCreator.

--- src/Orm/Reflection/DelegateCreator.cs
+++ src/Orm/Reflection/DelegateCreator.cs
@@ -5,6 +5,9 @@ using System.Reflection.Emit;
 
 namespace Orm.Reflection
 {
+  public delegate object GetValueDelegate(object instance);
+  public delegate void SetValueDelegate(object instance, object value);
+
   /// <summary>
   /// Custom <see cref="PropertyInfo"/> that wraps an existing property and provides
   /// <c>Reflection.Emit</c>-generated <see cref="GetValue"/> and <see cref="SetValue"/>
@@ -12,9 +15,6 @@ namespace Orm.Reflection
   /// invoke.
   /// http://www.codeproject.com/csharp/Fast_Dynamic_Properties.asp
   /// </summary>
-  /// 
-  public delegate void SetValueDelegate(object instance, object value);
-
   public class DelegateCreator
   {
     private static readonly Dictionary<Type, OpCode> TypeILCodeCache = new()
@@ -32,6 +32,40 @@ namespace Orm.Reflection
       [typeof(double)] = OpCodes.Ldind_R8,
       [typeof(float)] = OpCodes.Ldind_R4
     };
+
+    public GetValueDelegate CreatePropertyGetter(PropertyInfo property)
+    {
+      if (!property.CanRead)
+        throw new InvalidOperationException($"Property {property} has to be readable.");
+
+      var dm = new DynamicMethod("GetValueImpl", typeof(object), new Type[] { typeof(object) }, GetType().Module, false);
+      var ilgen = dm.GetILGenerator();
+
+      //.locals init (
+      //      object obj1)
+      LocalBuilder result = ilgen.DeclareLocal(typeof(object));
+      //L_0000: nop
+      ilgen.Emit(OpCodes.Nop);
+      //L_0001: ldarg.0
+      ilgen.Emit(OpCodes.Ldarg_0);
+      //L_0002: castclass [declaringType]
+      ilgen.Emit(OpCodes.Castclass, property.DeclaringType);
+      //L_0007: callvirt instance [declaringType] get_[B]()
+      ilgen.EmitCall(OpCodes.Callvirt, property.GetGetMethod(), null);
+
+      //Box if necessary:Yiyi
+      if (property.PropertyType.IsValueType)
+        ilgen.Emit(OpCodes.Box, property.PropertyType);
+
+      //L_000c: stloc.0
+      ilgen.Emit(OpCodes.Stloc_0, result);
+      //L_000f: ldloc.0
+      ilgen.Emit(OpCodes.Ldloc_0);
+      //L_0010: ret
+      ilgen.Emit(OpCodes.Ret);
+      
+      return (GetValueDelegate)dm.CreateDelegate(typeof(GetValueDelegate));
+    }
 
     public SetValueDelegate CreatePropertySetter(PropertyInfo property)
     {
