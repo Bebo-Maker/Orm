ac3e3c53963ff452341d49c204c3726c2c627b9c
Bebo-Maker <b.fenner@core-soft.de>
2021-03-09 19:38:43 +0100

    Add CreateUpdateBuilder to builder factory.

--- src/Orm/Factories/BuilderFactory.cs
+++ src/Orm/Factories/BuilderFactory.cs
@@ -10,6 +10,7 @@ namespace Orm.Factories
     public static string CreateSelectDistinctBuilder<T>(Action<IQueryBuilder<T>> action) => BuildBuilder(action, new SelectDistinctQueryBuilder<T>());
     public static string CreateDeleteBuilder<T>(Action<IQueryBuilder<T>> action) => BuildBuilder(action, new DeleteQueryBuilder<T>());
     public static string CreateInsertBuilder<T>(Action<IQueryBuilder<T>> action) => BuildBuilder(action, new InsertQueryBuilder<T>());
+    public static string CreateUpdateBuilder<T>(Action<IQueryBuilder<T>> action) => BuildBuilder(action, new UpdateQueryBuilder<T>());
 
     private static string BuildBuilder<T>(Action<IQueryBuilder<T>> action, QueryBuilder<T> builder)
     {
