439c310f09c78422f7428f1862b76e82d2f9703b
Bebo-Maker <b.fenner@core-soft.de>
2021-03-09 19:38:30 +0100

    Create UpdateQueryBuilder.cs

---
+++ src/Orm/Querying/Builders/UpdateQueryBuilder.cs
@@ -0,0 +1,15 @@
+﻿using Orm.Utils;
+
+namespace Orm.Querying.Builders
+{
+  public class UpdateQueryBuilder<T> : QueryBuilder<T>
+  {
+    public UpdateQueryBuilder()
+    {
+      _sb.Append("UPDATE ")
+         .Append(Table.Name)
+         .Append(" SET ")
+         .Append(SqlUtils.BuildUpdateParameters(Table));
+    }
+  }
+}
