348e9b0ccbe3a0c40472898dd5e91513334918dd
Bebo-Maker <b.fenner@core-soft.de>
2021-03-09 19:04:03 +0100

    DbFactory can create commands with parameters now.

--- src/Orm/Factories/BuilderFactory.cs
+++ src/Orm/Factories/BuilderFactory.cs
@@ -9,6 +9,7 @@ namespace Orm.Factories
     public static string CreateSelectBuilder<T>(Action<IQueryBuilder<T>> action) => BuildBuilder(action, new SelectQueryBuilder<T>());
     public static string CreateSelectDistinctBuilder<T>(Action<IQueryBuilder<T>> action) => BuildBuilder(action, new SelectDistinctQueryBuilder<T>());
     public static string CreateDeleteBuilder<T>(Action<IQueryBuilder<T>> action) => BuildBuilder(action, new DeleteQueryBuilder<T>());
+    public static string CreateInsertBuilder<T>(Action<IQueryBuilder<T>> action) => BuildBuilder(action, new InsertQueryBuilder<T>());
 
     private static string BuildBuilder<T>(Action<IQueryBuilder<T>> action, QueryBuilder<T> builder)
     {
--- src/Orm/Factories/DbFactory.cs
+++ src/Orm/Factories/DbFactory.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Orm.Entities;
+using Orm.Extensions;
+using Orm.Reflection;
+using System;
 using System.Data;
 using System.Data.Common;
 
@@ -10,6 +13,15 @@ namespace Orm.Factories
     {
       var command = connection.CreateCommand();
       command.CommandText = sqlStatement;
+      
+      return command;
+    }
+
+    public static IDbCommand CreateCommandWithParameters<T>(IDbConnection connection, string sqlStatement, T entity)
+    {
+      var command = CreateCommand(connection, sqlStatement);
+      var columns = TableFactory.GetOrCreateTableDefinition<T>().Columns;
+      AttachParams(command, columns, entity);
       return command;
     }
 
@@ -20,7 +32,22 @@ namespace Orm.Factories
         throw new InvalidOperationException($"{cmd} has to be DbCommand");
 
       command.CommandText = sqlStatement;
+
       return command;
+    }
+
+    public static DbCommand TryCreateAsyncCommandWithParameters<T>(IDbConnection connection, string sqlStatement, T entity)
+    {
+      var command = TryCreateAsyncComand(connection, sqlStatement);
+      var columns = TableFactory.GetOrCreateTableDefinition<T>().Columns;
+      AttachParams(command, columns, entity);
+      return command;
+    }
+
+    private static void AttachParams<T>(IDbCommand command, Column[] columns, T entity)
+    {
+      foreach (var column in columns)
+        command.AddParameter($"@{column.Alias}", column.GetValue(entity));
     }
   }
 }
