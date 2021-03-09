8e8d2d26d825c72a8d1d19fe462ff41c7b79e1da
Bebo-Maker <b.fenner@core-soft.de>
2021-03-09 19:39:01 +0100

    Add Update operations to IDBConnectionExtensions.

---
+++ src/Orm/IDbConnectionExtensions.Update.cs
@@ -0,0 +1,25 @@
+﻿using Orm.Factories;
+using Orm.Querying;
+using System;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace Orm
+{
+  public static partial class IDbConnectionExtensions
+  {
+    public static int Update<T>(this IDbConnection conn, T entity, Action<IQueryBuilder<T>> action = null)
+    {
+      var sql = BuilderFactory.CreateUpdateBuilder(action);
+      using var cmd = DbFactory.CreateCommandWithParameters(conn, sql, entity);
+      return cmd.ExecuteNonQuery();
+    }
+
+    public static Task<int> UpdateAsync<T>(this IDbConnection conn, T entity, Action<IQueryBuilder<T>> action = null)
+    {
+      var sql = BuilderFactory.CreateUpdateBuilder(action);
+      using var cmd = DbFactory.TryCreateAsyncCommandWithParameters(conn, sql, entity);
+      return cmd.ExecuteNonQueryAsync();
+    }
+  }
+}
--- src/Orm/Utils/SqlUtils.cs
+++ src/Orm/Utils/SqlUtils.cs
@@ -6,5 +6,6 @@ namespace Orm.Utils
   public static class SqlUtils
   {
     public static string BuildParameters(Table table) => string.Join(", ", table.Columns.Select(c => $"@{c.Alias}"));
+    public static string BuildUpdateParameters(Table table) => string.Join(", ", table.Columns.Select(c => $"{c.Alias} = @{c.Alias}"));
   }
 }
