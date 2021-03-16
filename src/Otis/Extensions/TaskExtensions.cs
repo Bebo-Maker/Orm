fc37cff53f8c9f755340cfbb226b586f10d79d97
Bebo-Maker <b.fenner@core-soft.de>
2021-03-16 18:03:40 +0100

    Create TaskExtensions.cs

---
+++ src/Otis/Extensions/TaskExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Otis.Extensions
+{
+  public static class TaskExtensions
+  {
+    /// <inheritdoc cref="Enumerable.FirstOrDefault{TSource}(IEnumerable{TSource})" />
+    public static async Task<T> FirstOrDefault<T>(this Task<IEnumerable<T>> task) 
+      => (await task.ConfigureAwait(false)).FirstOrDefault();
+
+    /// <inheritdoc cref="Enumerable.FirstOrDefault{TSource}(IEnumerable{TSource})" />
+    public static async Task<T> FirstOrDefault<T>(this Task<List<T>> task)
+      => (await task.ConfigureAwait(false)).FirstOrDefault();
+  }
+}
